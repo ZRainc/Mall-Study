4f80dd1e616b302f72207a6632cedabef22ab24d
zrainc <823731949@qq.com>
2021-01-26 14:17:25 +0800

    完成验证码登录功能

--- mall-redis/src/main/java/com/zrainc/mall/controller/UmsMemberController.java
+++ mall-redis/src/main/java/com/zrainc/mall/controller/UmsMemberController.java
@@ -1,9 +1,15 @@
 package com.zrainc.mall.controller;
 
+import com.zrainc.mall.common.api.CommonResult;
+import com.zrainc.mall.service.UmsMemberService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created with IntelliJ IDEA.
@@ -17,4 +23,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Api(tags = "UmsMemberController", description = "会员登录注册管理")
 @RequestMapping("/sso")
 public class UmsMemberController {
+    @Autowired
+    private UmsMemberService umsMemberService;
+
+    @ApiOperation("获取验证码")
+    @RequestMapping(value= "/getAuthCode", method = RequestMethod.GET)
+    public CommonResult getAuthCode(@RequestParam String telephone) {
+        return umsMemberService.generateAuthCode(telephone);
+    }
+
+    @ApiOperation("判断验证码是否正确")
+    @RequestMapping(value= "/verifyAuthCode", method = RequestMethod.POST)
+    public CommonResult verifyAuthCode(@RequestParam String telephone, @RequestParam String authCode) {
+        return umsMemberService.verifyAuthCode(telephone, authCode);
+    }
 }
